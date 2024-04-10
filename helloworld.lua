2d347f3a29ff68a616258093bf736047c5439b98
Waleed Amgad <waleedbookey@gmail.com>
2024-04-10 15:46:25 +0200

    simple conditionals

--- helloworld.lua
+++ helloworld.lua
@@ -4,4 +4,12 @@ print("hello world")
 io.write("hello world,from",_VERSION,"!\n")
 local int Age=15
 --this is a comment--
-print(Age)
+io.write("please input a number")
+ Num=io.read("n")
+ local num=io.read("n")
+if(Num==5)
+then io.write("5 was entered") 
+else
+    io.write("5 was not entered")
+
+end
