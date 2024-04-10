9bc1c067eadbaf98ed1d9327db92098ccd0d35da
Waleed Amgad <waleedbookey@gmail.com>
2024-04-10 15:57:11 +0200

    stupid

--- helloworld.lua
+++ helloworld.lua
@@ -13,3 +13,21 @@ else
     io.write("5 was not entered")
 
 end
+
+local a,b
+local  tmp
+a=io.read("n")
+b=io.read("n")
+
+print("value of a :",a)
+print("value of b:",b)
+
+tmp=b
+b=a
+a=tmp
+
+print("value of new a",a)
+print("value of new b",b)
+
+tmp=a/b
+print("tmp")
